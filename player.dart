06805d8280a7103cdb4f8b7f5df27ed8e883379e
jaygarcia <jesusdgarcia@gmail.com>
2021-06-09 13:23:29 -0400

    Added new waveform view

--- player.dart
+++ player.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:dart_openmpt/dart_openmpt.dart';
 import 'package:dart_openmpt/dart_openmpt_structs.dart';
 import 'package:ffi/ffi.dart';
-
 import 'package:ansicolor/ansicolor.dart';
 
 // Colored "AnsiPen" instances to draw characters
@@ -11,11 +10,15 @@ AnsiPen redPen = new AnsiPen()..red();
 AnsiPen bluePen = new AnsiPen()..blue();
 AnsiPen purplePen = new AnsiPen()..magenta();
 AnsiPen grayPen = new AnsiPen()..gray();
+AnsiPen greenPen =  new AnsiPen()..green();
+AnsiPen yellowPen =  new AnsiPen()..yellow();
 AnsiPen blueBgPen = new AnsiPen()..blue(bg:true);
 
 // Colored Strings used to plot the Audio wave forms
 final String leftDot = bluePen('▓');
+final String leftDotAlt = yellowPen('▓');
 final String rightDot = redPen('▓');
+final String rightDotAlt = greenPen('▓');
 final String hyphen = grayPen('─');
 final String pipeChar = grayPen('│');
 final String spaceChar = ' ';
@@ -48,8 +51,215 @@ int midY = 0;
 int midX = 0;
 List<List<String>> allPatterns = [['']];
 
+/*
+   Create memory space to act as a psuedo screen buffer. While allocating the
+   arrays of Strings, we add '-' or '|' (pipe character) to draw the X and Y
+   axes.
+   Is there a more efficient way of doing this?
+*/
+List<List<String>> newWaveformScreenBuffer(int numCols, int numRows) {
+
+  List<List<String>> screenBuffer = [];
+
+  for (int rowNum = 0; rowNum < numRows; rowNum++) {
+    List<String> row = [];
+    for (int col = 0; col < numCols; col++) {
+
+      String str = spaceChar;
+      if (rowNum == midY) {
+        str = hyphen;
+      }
+
+      if (col == midX) {
+        str = pipeChar;
+      }
+      row.add(str);
+    }
+
+    screenBuffer.add(row);
+  }
+
+  return screenBuffer;
+}
+
+void drawLiveWaveform(List<List<String>> screenBuffer, StereoAudioBuffers buffers, int numCols, int numRows) {
+
+  // We use samplesPerDot to create averages of values so the waveform can make
+  // use of the full 512 values per stereo channel.
+  int samplesPerDot = (buffers.num_items / (numCols / 2)).floor();
+
+  // LEFT channel
+  for (int col = 0; col < midX; col++) {
+    double leftSum = 0;
+    for (int sampleIdx = 0; sampleIdx < samplesPerDot; sampleIdx++) {
+
+      int ltIndex = (col * samplesPerDot) +  sampleIdx;
+      if (ltIndex > buffers.left_buffer.length - 1) {
+        ltIndex = buffers.left_buffer.length - 1;
+      }
+
+      leftSum += buffers.left_buffer[ltIndex];;
+      if (leftSum.isNaN) { // TODO: Why do we get NaN sometimes? :(
+        leftSum = 0; // Hack
+      }
+    }
+
+    double leftAverage = leftSum / samplesPerDot;
+    int leftChannelY = midY + (leftAverage * numRows / 2).floor();
+
+    // Set the ceiling.
+    if (leftChannelY < 0) {
+      leftChannelY = 0;
+    }
+
+    // Set the floor.
+    if (leftChannelY > numRows) {
+      leftChannelY = numRows - 1;
+    }
+
+    // Plot the value in the psuedo screen buffer.
+    screenBuffer[leftChannelY][col] = leftDot;
+  }
+  // RIGHT channel
+  int iterationNum = 0;
+  for (int drawCol = midX + 1; drawCol < numCols; drawCol++) {
+    double rightSum = 0;
+
+    for (int sampleIdx = 0; sampleIdx < samplesPerDot; sampleIdx++) {
+      int rtIndex = (iterationNum * samplesPerDot) + sampleIdx;
+      if (rtIndex > buffers.right_buffer.length - 1) {
+        rtIndex = buffers.right_buffer.length - 1;
+      }
+
+      double rtBufferVal = buffers.right_buffer[rtIndex];
+      rightSum += rtBufferVal;
+      if (rightSum.isNaN) {
+        rightSum = 0; // Hack
+      }
+    }
+
+    double rightAverage = rightSum / samplesPerDot;
+    int rightChannelY = midY + (rightAverage * numRows / 2).floor();
+
+    // Set the ceiling.
+    if (rightChannelY < 0) {
+      rightChannelY = 0;
+    }
+
+    // Set the floor.
+    if (rightChannelY > numRows) {
+      rightChannelY = numRows - 1;
+    }
+
+    // Plot the value in the psuedo screen buffer.
+    screenBuffer[rightChannelY][drawCol] = rightDot;
+    iterationNum++;
+  }
+
+}
+
+List<int> priorLeftValues = [];
+List<int> priorRightValues = [];
+
+void drawTimedWaveform(List<List<String>> screenBuffer, StereoAudioBuffers buffers, int numCols, int numRows) {
+  int numValues = priorLeftValues.length - 1;
+  int floor = ((numRows) * .9).floor();
+
+  // If we grew horizontally
+  if (numValues < midX) {
+    for (int i = numValues; i < midX; i++) {
+      priorLeftValues.add(0);
+      priorRightValues.add(0);
+    }
+  }
+
+  // If we shrank horizontally
+  if (numValues > midX) {
+    for (int i = 0; i < numValues - midX; i++) {
+      priorLeftValues.removeAt(0);
+      priorRightValues.removeAt(0);
+    }
+  }
+
+  /**** L E F T   V A L U E S  ****/
+  double leftValue = 0;
+  for (int i = 0; i < buffers.left_buffer.length - 1; i++) {
+    leftValue += buffers.left_buffer[i].abs();
+  }
+
+  leftValue = (leftValue * 2) / buffers.left_buffer.length;
+  priorLeftValues.removeAt(0);
+  priorLeftValues.add((leftValue * midY).floor());
+
+  for (int col = 0; col < priorLeftValues.length - 1; col++) {
+    int val = priorLeftValues[col];
+    int leftChannelY = midY + val;
+    // Floor (bottom)
+    if (leftChannelY >= floor) {
+      leftChannelY = floor;
+    }
+
+    // Ceiling (top)
+    if (leftChannelY < 0) {
+      leftChannelY = 0;
+    }
+
+    screenBuffer[leftChannelY][col] = leftDot;
+
+    int yStop = (leftChannelY - midY) + 1;
+    for (int y = leftChannelY; y > (midY - yStop); y--) {
+      screenBuffer[y][col] = leftDot;
+    }
+  }
+
+  /**** R I G H T   V A L U E S ****/
+
+  double rightValue = 0;
+  for (int i = 0; i < buffers.right_buffer.length - 1; i++) {
+    rightValue += buffers.right_buffer[i].abs();
+  }
+
+  rightValue = (rightValue * 2) / buffers.right_buffer.length;
+  priorRightValues.removeAt(0);
+  priorRightValues.add((rightValue * midY).floor());
+
+  int index = 0;
+
+  for (int col = midX + 1; col < numCols; col++) {
+    int val = priorRightValues[index];
+    int rightChannelY = midY + val;
+    // Floor (bottom)
+    if (rightChannelY >= floor) {
+      rightChannelY = floor;
+    }
+
+    // Ceiling (top)
+    if (rightChannelY < 0) {
+      rightChannelY = 0;
+    }
+
+    screenBuffer[rightChannelY][col] = leftDot;
+
+    int yStop = (rightChannelY - midY) + 1;
+    for (int y = rightChannelY; y > (midY - yStop); y--) {
+      screenBuffer[y][col] = rightDot;
+    }
+
+    index++;
+  }
+}
+
+
+enum mode_type {
+  LIVE_WAVEFORM,
+  TIMED_WAVEFORM
+}
+
+// Used to toggle modes
+int mode = mode_type.LIVE_WAVEFORM.index;
+
 // Utility to draw the waveforms on screen
-void drawBuffers(OpenMpt openMpt) {
+void drawAudioBuffers(OpenMpt openMpt) {
 
   ModPosition pos = openMpt.getModPosition();
   StereoAudioBuffers buffers = openMpt.getStereoAudioBuffers();
@@ -99,114 +309,23 @@ void drawBuffers(OpenMpt openMpt) {
     else {
       print(patternbuffer[idx]);
     }
-
-  };
+  }
 
   // Add one line underneath the pattern view
   print('');
 
+  // Destroy prior array and start anew.
+  List<List<String>> screenBuffer = newWaveformScreenBuffer(numCols, numRows);
 
-  //TODO: Investigate reuse versus recreation/destruction everytime this function is run.
-  List<List<String>> screenBuffer = [];
-
-  /* Create memory space to act as a psuedo screen buffer. While allocating the
-     arrays of Strings, we add '-' or '|' (pipe character) to draw the X and Y
-     axes.
-     Is there a more efficient way of doing this?
-   */
-  for (int rowNum = 0; rowNum < numRows; rowNum++) {
-    List<String> row = [];
-    for (int col = 0; col < numCols; col++) {
-
-      String str = spaceChar;
-      if (rowNum == midY) {
-        str = hyphen;
-      }
-
-      if (col == midX) {
-        str = pipeChar;
-      }
-      row.add(str);
-    }
-
-    screenBuffer.add(row);
+  if (mode == mode_type.LIVE_WAVEFORM.index) {
+    // Draw the live waveform
+    drawLiveWaveform(screenBuffer, buffers, numCols, numRows);
+  }
+  else if (mode == mode_type.TIMED_WAVEFORM.index){
+    drawTimedWaveform(screenBuffer, buffers, numCols, numRows);
   }
 
-  // We use samplesPerDot to create averages of values so the waveform can make
-  // use of the full 512 values per stereo channel.
-  int samplesPerDot = (buffers.num_items / (numCols / 2)).floor();
-
-  // LEFT channel
-  for (int col = 0; col < midX; col++) {
-    double leftSum = 0;
-    for (int sampleIdx = 0; sampleIdx < samplesPerDot; sampleIdx++) {
-
-      int ltIndex = (col * samplesPerDot) +  sampleIdx;
-      if (ltIndex > buffers.left_buffer.length - 1) {
-        ltIndex = buffers.left_buffer.length - 1;
-      }
-
-      leftSum += buffers.left_buffer[ltIndex];;
-      if (leftSum.isNaN) { // TODO: Why do we get NaN sometimes? :(
-        leftSum = 0; // Hack
-      }
-    }
-
-    double leftAverage = leftSum / samplesPerDot;
-    int leftChannelY = midY + (leftAverage * numRows / 2).floor();
-
-    // Set the ceiling.
-    if (leftChannelY < 0) {
-      leftChannelY = 0;
-    }
-
-    // Set the floor.
-    if (leftChannelY > numRows) {
-      leftChannelY = numRows - 1;
-    }
-
-    // Plot the value in the psuedo screen buffer.
-    screenBuffer[leftChannelY][col] = leftDot;
-  }
-
-  // RIGHT channel
-  int iterationNum = 0;
-  for (int drawCol = midX + 1; drawCol < numCols; drawCol++) {
-    double rightSum = 0;
-
-    for (int sampleIdx = 0; sampleIdx < samplesPerDot; sampleIdx++) {
-      int rtIndex = (iterationNum * samplesPerDot) + sampleIdx;
-      if (rtIndex > buffers.right_buffer.length - 1) {
-        rtIndex = buffers.right_buffer.length - 1;
-      }
-
-      double rtBufferVal = buffers.right_buffer[rtIndex];
-      rightSum += rtBufferVal;
-      if (rightSum.isNaN) {
-        rightSum = 0; // Hack
-      }
-    }
-
-    double rightAverage = rightSum / samplesPerDot;
-    int rightChannelY = midY + (rightAverage * numRows / 2).floor();
-
-    // Set the ceiling.
-    if (rightChannelY < 0) {
-      rightChannelY = 0;
-    }
-
-    // Set the floor.
-    if (rightChannelY > numRows) {
-      rightChannelY = numRows - 1;
-    }
-
-    // Plot the value in the psuedo screen buffer.
-    screenBuffer[rightChannelY][drawCol] = rightDot;
-    iterationNum++;
-  }
-
-
-  // Print the buffer
+  // Print screen buffer
   for (int rowNum = 0; rowNum < numRows; rowNum++) {
     print(screenBuffer[rowNum].join(''));
   }
@@ -217,9 +336,11 @@ OpenMpt openMpt = OpenMpt();
 
 void updateView() {
   if (shouldContinue) {
+
+
     final stopwatch = Stopwatch()..start();
 
-    drawBuffers(openMpt);
+    drawAudioBuffers(openMpt);
     int diff = 20 - stopwatch.elapsed.inMilliseconds;
     // sleep ONLY if we need to.
     if (diff < 0) {
@@ -247,6 +368,15 @@ main(List<String> args)  {
   // Begin Drawing
   updateView();
 
+  // Read stdin and allow any key press to change the mode
+  stdin.lineMode = false;
+  stdin.echoMode = false;
+
+  stdin.forEach((element) {
+    mode = (mode == mode_type.LIVE_WAVEFORM.index)
+        ? mode_type.TIMED_WAVEFORM.index : mode_type.LIVE_WAVEFORM.index;
+  });
+
   ProcessSignal.sigint.watch().forEach((signal) {
     shouldContinue = false;
     clearScreen(hard:true);
@@ -258,7 +388,6 @@ main(List<String> args)  {
     openMpt.shutdown();
     exit(0);
   });
-
 
 }
 
